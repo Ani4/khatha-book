b94d84a2bf5ef0524bcc7f46cc5798141116312f
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-09-30 08:10:47 +0530

    dummy data updated

--- bill_ocr/src/containers/InnerDashboard/Report.jsx
+++ bill_ocr/src/containers/InnerDashboard/Report.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Bar, Doughnut, Line, Pie } from "react-chartjs-2";
-const data = {
+const Monthdata = {
     labels: [
         "January",
         "February",
@@ -19,18 +19,17 @@ const data = {
         {
             label: "Month",
             data: [
-                300,
-                500,
-                1100,
-                3100,
-                550,
-                2300,
-                3500,
-                520,
-                1500,
-                3400,
-                500,
-                1000,
+                30050,
+                504560,
+                11400,
+                365100,
+                125450,
+                253400,
+                135020,
+                555200,
+                30050,
+                504560,
+                11400,
             ],
             backgroundColor: [
                 "#DB2BC0",
@@ -47,18 +46,43 @@ const data = {
                 "#EB7693",
                 "#280E16",
             ],
-            hoverBackgroundColor: [],
+            hoverBackgroundColor: [
+                "#DB2BC0",
+                "#1F76FC",
+                "#129253",
+                "#BA5A24",
+                "#657565",
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#23D7C8",
+                "#2E2773",
+                "#5875AF",
+                "#EB7693",
+                "#280E16",
+            ],
         },
     ],
 };
 
+const CustomerData = {
+    labels: ["Aniket", "Jagdish", "Anuj", "Amit"],
+    datasets: [
+        {
+            label: "Sale",
+            data: [300, 500, 1100, 3100],
+            backgroundColor: ["#2E2773", "#5875AF", "#EB7693", "#280E16"],
+            hoverBackgroundColor: ["#2E2773", "#5875AF", "#EB7693", "#280E16"],
+        },
+    ],
+};
 function Report() {
     return (
         <div>
             <section class="page-content">
                 <section class="grid">
-                    {/* <Pie
-                        data={data}
+                    <Line
+                        data={Monthdata}
                         width={5}
                         height={200}
                         options={{
@@ -66,22 +90,24 @@ function Report() {
                             legend: { display: false },
                             title: {
                                 display: true,
-                                text: "Hurray",
-                                fontSize: 24,
+                                text: "Sales vs Months",
+                                fontSize: 18,
+                                position: "bottom",
                             },
                         }}
-                    /> */}
-                    <Line
-                        data={data}
-                        width={5}
-                        height={200}
+                    />
+                </section>
+                <hr />
+                <section class="grid">
+                    <Pie
+                        data={CustomerData}
                         options={{
-                            maintainAspectRatio: false,
                             legend: { display: false },
                             title: {
                                 display: true,
-                                text: "Sales accordingly months",
-                                fontSize: 24,
+                                text: "Sales vs Customer",
+                                fontSize: 18,
+                                position: "bottom",
                             },
                         }}
                     />
